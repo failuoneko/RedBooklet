d01d294ca5552b7450a770735e584a9e2f4a213d
Luo <failuoneko@gmail.com>
2023-01-04 00:27:03 +0800

    <feat> init main

---
+++ RedBooklet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedBooklet
+//
+//  Created by L on 2023/1/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
